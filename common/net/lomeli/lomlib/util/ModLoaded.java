4c970326a037ddc184ce321c47333d150bbc6b69
Lomeli12 <anthonylomeli11@gmail.com>
2013-05-16 21:41:10 -0700

    Overloaded the a function

--- common/net/lomeli/lomlib/util/ModLoaded.java
+++ common/net/lomeli/lomlib/util/ModLoaded.java
@@ -33,4 +33,29 @@ public class ModLoaded
 		
 		return isInstalled;
 	}
+	
+	public static boolean isModInstalled(String modID)
+	{
+		boolean isInstalled = false;
+		if (Loader.isModLoaded(modID))
+        {
+            try
+            {
+            	LomLib.logger.log(Level.FINE, (modID + " is installed!"));
+            	isInstalled = true;
+            }
+            catch(Exception ex)
+            {
+            	LomLib.logger.log(Level.WARNING, (modID + " is not installed!"));
+            	isInstalled = false;
+            }
+        }
+		else
+		{
+			LomLib.logger.log(Level.WARNING, (modID + " is not installed!"));
+			isInstalled = false;
+		}
+		
+		return isInstalled;
+	}
 }
