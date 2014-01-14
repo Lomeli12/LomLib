a4df00a84d051ea72ffd899b6fc366f8d56f5225
Lomeli12 <anthonylomeli11@gmail.com>
2014-01-13 17:27:50 -0800

    Stuff!

--- src/main/java/net/lomeli/lomlib/asm/LomLibPlugin.java
+++ src/main/java/net/lomeli/lomlib/asm/LomLibPlugin.java
@@ -4,9 +4,7 @@ import java.io.File;
 import java.util.Map;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
-import cpw.mods.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
 
-@TransformerExclusions({ "net.lomeli.lomlib.asm" })
 public class LomLibPlugin implements IFMLLoadingPlugin {
     public static File location;
 
