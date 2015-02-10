ba9780640303de2c25e19f91904722a936fc532a
Lomeli12 <anthonylomeli11@gmail.com>
2015-02-09 17:30:10 -0800

    Fixed build.gradle and some other things.

--- src/main/java/net/lomeli/lomlib/client/patreon/PatreonCrown.java
+++ src/main/java/net/lomeli/lomlib/client/patreon/PatreonCrown.java
@@ -15,39 +15,32 @@ public class PatreonCrown extends ModelBase {
     public ModelRenderer baseCrown3;
 
     public PatreonCrown() {
-        this.textureWidth = 32;
-        this.textureHeight = 32;
-        this.baseCrown3 = new ModelRenderer(this, 0, -2);
-        this.baseCrown3.setRotationPoint(0.0F, 12.0F, 2.0F);
-        this.baseCrown3.addBox(0.0F, 0.0F, 0.0F, 0, 4, 5, 0.0F);
-        this.setRotateAngle(baseCrown3, -0.3490658503988659F, 0.0F, 0.0F);
-        this.baseCrown2 = new ModelRenderer(this, 0, -1);
-        this.baseCrown2.setRotationPoint(-5.0F, 13.71F, 6.71F);
-        this.baseCrown2.addBox(0.0F, 0.0F, 0.0F, 5, 4, 0, 0.0F);
-        this.setRotateAngle(baseCrown2, -0.3490658503988659F, 0.0F, 0.0F);
-        this.baseCrown1 = new ModelRenderer(this, 0, -2);
-        this.baseCrown1.setRotationPoint(-5.0F, 12.0F, 2.0F);
-        this.baseCrown1.addBox(0.0F, 0.0F, 0.0F, 0, 4, 5, 0.0F);
-        this.setRotateAngle(baseCrown1, -0.3490658503988659F, 0.0F, 0.0F);
+        this.textureWidth = 16;
+        this.textureHeight = 16;
         this.baseCrown = new ModelRenderer(this, 0, -1);
         this.baseCrown.setRotationPoint(-5.0F, 12.0F, 2.0F);
         this.baseCrown.addBox(0.0F, 0.0F, 0.0F, 5, 4, 0, 0.0F);
         this.setRotateAngle(baseCrown, -0.3490658503988659F, 0.0F, 0.0F);
+        this.baseCrown1 = new ModelRenderer(this, 0, -2);
+        this.baseCrown1.setRotationPoint(-5.0F, 12.0F, 2.0F);
+        this.baseCrown1.addBox(0.0F, 0.0F, 0.0F, 0, 4, 5, 0.0F);
+        this.setRotateAngle(baseCrown1, -0.3490658503988659F, 0.0F, 0.0F);
+        this.baseCrown2 = new ModelRenderer(this, 0, -1);
+        this.baseCrown2.setRotationPoint(-5.0F, 13.71F, 6.71F);
+        this.baseCrown2.addBox(0.0F, 0.0F, 0.0F, 5, 4, 0, 0.0F);
+        this.setRotateAngle(baseCrown2, -0.3490658503988659F, 0.0F, 0.0F);
+        this.baseCrown3 = new ModelRenderer(this, 0, -2);
+        this.baseCrown3.setRotationPoint(0.0F, 12.0F, 2.0F);
+        this.baseCrown3.addBox(0.0F, 0.0F, 0.0F, 0, 4, 5, 0.0F);
+        this.setRotateAngle(baseCrown3, -0.3490658503988659F, 0.0F, 0.0F);
     }
 
     @Override
     public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
-        this.baseCrown3.render(f5);
-        this.baseCrown2.render(f5);
-        this.baseCrown1.render(f5);
         this.baseCrown.render(f5);
-    }
-
-    public void render(float f) {
-        this.baseCrown3.render(f);
-        this.baseCrown2.render(f);
-        this.baseCrown1.render(f);
-        this.baseCrown.render(f);
+        this.baseCrown1.render(f5);
+        this.baseCrown2.render(f5);
+        this.baseCrown3.render(f5);
     }
 
     /**
@@ -57,5 +50,12 @@ public class PatreonCrown extends ModelBase {
         modelRenderer.rotateAngleX = x;
         modelRenderer.rotateAngleY = y;
         modelRenderer.rotateAngleZ = z;
+    }
+
+    public void render(float f) {
+        this.baseCrown3.render(f);
+        this.baseCrown2.render(f);
+        this.baseCrown1.render(f);
+        this.baseCrown.render(f);
     }
 }
